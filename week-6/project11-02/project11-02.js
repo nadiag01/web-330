55a741b32f37443544186a0ccb90b89e448ffccc
Nadia <nadiagainer@gmail.com>
2024-04-28 20:13:33 -0500

    update

--- week-6/project11-02/project11-02.js
+++ week-6/project11-02/project11-02.js
@@ -4,8 +4,8 @@
       Project 11-02
 
       Project to city and state information from a provided postal code
-      Author: 
-      Date:   
+      Author: Nadia Gainer
+      Date:   04/28/24
 
       Filename: project11-02.js
 */
@@ -16,7 +16,17 @@ let region = document.getElementById("region");
 let country = document.getElementById("country");
 
 postalCode.onblur = function() {
- 
+const codeValue= postalCode.value
+const countryValue= country.value
+place.value=""
+region.value=""
+fetch(`http://api.zippopotam.us/${countryValue}/${codeValue}`)
+.then(response => response.json())
+.then(json => {
+      place.value = json.places[0]["place name"]
+      region.value= json.places[0]["state abbreviation"]
+})
+.catch(error => console.log(error))
 }
 
 
